ffb42b9eb6b907fe744188310858ee08abe4f5c0
Gabriel Lungu <gabri052000@gmail.com>
2024-04-01 10:11:09 +0200

    Redirect to login 3.0

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -4,7 +4,7 @@ import LogoLight from '../media/logoLight.png';
 
 function Logo() {
   return (
-    <Link to="/" className=" flex justify-center py-6">
+    <Link to="/home" className=" flex justify-center py-6">
       <img src={LogoLight} alt="Logo" className="h-28" />
     </Link>
   );
